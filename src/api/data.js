0abae1366df36c2062c775bb8124616120845f1b
DestroyerBg <99879227+DestroyerBg@users.noreply.github.com>
2025-02-06 19:35:04 +0200

    Fixed tests

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'https://library-15ey.onrender.com'
-api.settings.host = 'https://library-15ey.onrender.com';
+const host = 'http://localhost:3030'
+api.settings.host = 'http://localhost:3030';
 
 export const login = api.login;
 export const register = api.register;
